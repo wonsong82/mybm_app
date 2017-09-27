970f581c5009a99994dbe68ef77edc73669a07d6
won song <wonsong82@gmail.com>
2017-09-27 18:26:39 -0400

    chaged birthday 투 한글

--- app/resources/views/birthday.blade.php
+++ app/resources/views/birthday.blade.php
@@ -15,7 +15,7 @@
                 <div class="panel-body">
                     <ul>
                         @foreach($bd['users'] as $user)
-                        <li>{{ date('n/j (D)', strtotime($user->birthdayThisYear)) }} {{ $user->profile->name }}</li>
+                        <li>{{ date('n월 j일 (D)', strtotime($user->birthdayThisYear)) }} {{ $user->profile->name }}</li>
                         @endforeach
                     </ul>
                 </div>
